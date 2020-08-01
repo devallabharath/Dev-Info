b9434cba34bedabebca0eb12db85887d2e0454cb
devallabharath <55385708+devallabharath@users.noreply.github.com>
2020-08-01 13:58:06 +0530

    added javascript and rating

---
+++ Script.js
@@ -0,0 +1,43 @@
+//  JavaScript for Dev-Info.html
+
+// Rating
+
+function s1 () {
+	document.getElementById("s1").style.color="yellow";
+	document.getElementById("s2").style.color="white";
+	document.getElementById("s3").style.color="white";
+	document.getElementById("s4").style.color="white";
+	document.getElementById("s5").style.color="white";
+}
+
+function s2 () {
+	document.getElementById("s1").style.color="yellow";
+	document.getElementById("s2").style.color="yellow";
+	document.getElementById("s3").style.color="white";
+	document.getElementById("s4").style.color="white";
+	document.getElementById("s5").style.color="white";
+}
+
+function s3 () {
+	document.getElementById("s1").style.color="yellow";
+	document.getElementById("s2").style.color="yellow";
+	document.getElementById("s3").style.color="yellow";
+	document.getElementById("s4").style.color="white";
+	document.getElementById("s5").style.color="white"
+}
+
+function s4 () {
+	document.getElementById("s1").style.color="yellow";
+	document.getElementById("s2").style.color="yellow";
+	document.getElementById("s3").style.color="yellow";
+	document.getElementById("s4").style.color="yellow";
+	document.getElementById("s5").style.color="white";
+}
+
+function s5 () {
+	document.getElementById("s1").style.color="yellow";
+	document.getElementById("s2").style.color="yellow";
+	document.getElementById("s3").style.color="yellow";
+	document.getElementById("s4").style.color="yellow";
+	document.getElementById("s5").style.color="yellow";
+}
